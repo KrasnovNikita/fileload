2274ee05410c4c10574024858e368701a9804471
Krasnov Nikita <nkrasnov@datas-tech.com>
2018-03-22 14:45:00 +0200

    Initial commit

---
+++ src/main/java/com/datatech/oschad/fileload/FileloadApplication.java
@@ -0,0 +1,12 @@
+package com.datatech.oschad.fileload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileloadApplication.class, args);
+	}
+}
